d183a9f13f15a18cc711d5fbc68eca9734893625
Manish523 <manish.callme@gmail.com>
2020-10-11 15:18:32 +0530

    End of section 11

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -5,7 +5,6 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { ShopComponent } from './shop.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
 import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
