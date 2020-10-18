88c5770283cc25ce424d3ac46b22de3b34ffdfa9
Manish523 <manish.callme@gmail.com>
2020-10-18 16:57:27 +0530

    End of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
@@ -26,9 +27,31 @@ export class AppComponent implements OnInit {
   // );
   // }
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(
+        () => {
+          console.log('loaded user');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  loadBasket() {
     const basketID = localStorage.getItem('basket_id');
     if (basketID) {
       this.basketService.getBasket(basketID).subscribe(
--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -16,7 +16,10 @@ export class LoadingInterceptors implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    this.busyService.busy();
+    if (!req.url.includes('emailexists')) {
+      this.busyService.busy();
+    }
+    // this.busyService.busy();
     return next.handle(req).pipe(
       delay(1000),
       finalize(() => {
